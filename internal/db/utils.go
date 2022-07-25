3c57278630c42ca8ab8f1c1b08927cc295594d45
Yen Ter <leeyenter@gmail.com>
2022-07-25 20:22:34 +0800

    Clean up error handling

--- internal/db/utils.go
+++ internal/db/utils.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
-	"github.com/kaphos/webapp/internal/errorhandling"
+	"github.com/kaphos/webapp/pkg/errchk"
 	"os"
 )
 
-func getenv(key, fallback string) string {
+func getEnv(key, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		return fallback
@@ -18,25 +18,24 @@ func getenv(key, fallback string) string {
 }
 
 func getDBConnStr(defaultUser, defaultPass string) string {
-	dbUser := getenv("DB_USER", defaultUser)
-	dbPass := getenv("DB_PASS", defaultPass)
-	dbHost := getenv("DB_HOST", "127.0.0.1")
-	dbPort := getenv("DB_PORT", "5432")
+	dbUser := getEnv("DB_USER", defaultUser)
+	dbPass := getEnv("DB_PASS", defaultPass)
+	dbHost := getEnv("DB_HOST", "127.0.0.1")
+	dbPort := getEnv("DB_PORT", "5432")
 	return fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%s", dbUser, dbUser, dbPass, dbHost, dbPort)
 }
 
-// convertUserError returns a defined errchk (errorhandling.ErrClientSide) if the errchk code
+// convertUserError returns a defined errchk (errchk.ErrClientSide) if the errchk code
 // falls into a predefined set, that is due to user input errchk (e.g. duplicate).
 func convertUserError(err error) error {
-	if err != nil && err != pgx.ErrNoRows {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) {
-			if pgErr.Code == "23503" || pgErr.Code == "23505" {
-				return errorhandling.ErrClientSide
-			}
-		}
-	} else if err == pgx.ErrNoRows {
-		return errorhandling.ErrNoRows
+	if err == nil || err == pgx.ErrNoRows || err == pgx.ErrTxClosed {
+		return nil
 	}
+
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) && (pgErr.Code == "23503" || pgErr.Code == "23505") {
+		return errchk.ErrClientSide
+	}
+
 	return err
 }
