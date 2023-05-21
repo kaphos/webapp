e93656796148aa2aefa3841f230f9e5eb183debc
Yen Ter <leeyenter@gmail.com>
2023-05-21 22:16:06 +0800

    Add test for healthcheck

--- example/main_test.go
+++ example/main_test.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"github.com/kaphos/webapp"
+	"github.com/stretchr/testify/assert"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
 	m.Run()
+}
+
+func TestHealthcheck(t *testing.T) {
+	s, w := setup()
+	req, _ := http.NewRequest("GET", "/healthcheck", nil)
+	s.Router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
 }
 
 func setup() (*webapp.Server, *httptest.ResponseRecorder) {
